1580931059194e2b54e793bd84591fc1659f12f5
SakuniJayasinghe <s.nimnadi@gmail.com>
2024-09-01 00:57:51 +0530

    Updates

---
+++ precache-manifest.fda2dfc095f05de2c64b25a2508ded07.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42aec7b52d206b32f581227302a3fe46",
+    "url": "/CO528-angle-selector-React/index.html"
+  },
+  {
+    "revision": "209d1975f0007be648fa",
+    "url": "/CO528-angle-selector-React/static/css/main.ab9491f0.chunk.css"
+  },
+  {
+    "revision": "1582e325d1bcf091e744",
+    "url": "/CO528-angle-selector-React/static/js/2.ca150098.chunk.js"
+  },
+  {
+    "revision": "2e7b5277f70c872aecfe",
+    "url": "/CO528-angle-selector-React/static/js/3.940f5f6f.chunk.js"
+  },
+  {
+    "revision": "209d1975f0007be648fa",
+    "url": "/CO528-angle-selector-React/static/js/main.e5039932.chunk.js"
+  },
+  {
+    "revision": "77f96a0449e168667317",
+    "url": "/CO528-angle-selector-React/static/js/runtime~main.a3a323d1.js"
+  }
+]);
